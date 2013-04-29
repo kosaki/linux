00d409086814951e546cf6c94662e2758a2e1805
KOSAKI Motohiro <kosaki.motohiro@jp.fujitsu.com>
2013-04-29 14:01:00 -0400

    sched: task_sched_runtime introduce micro optimization
    rq lock in task_sched_runtime() is necessary for two reasons. 1)
    accessing se.sum_exec_runtime is not atomic on 32bit and 2)
    do_task_delta_exec() require it.
    
    So, 64bit can avoid holding rq lock when add_delta is false and
    delta_exec is 0.
    
    Cc: Olivier Langlois <olivier@trillion01.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Ingo Molnar <mingo@kernel.org>
    Suggested-by: Paul Turner <pjt@google.com>
    Acked-by: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: KOSAKI Motohiro <kosaki.motohiro@jp.fujitsu.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2692,6 +2692,21 @@ unsigned long long task_sched_runtime(struct task_struct *p, bool add_delta)
 	struct rq *rq;
 	u64 ns = 0;
 
+#ifdef CONFIG_64BIT
+	/*
+	 * 64-bit doesn't need locks to atomically read a 64bit value. So we
+	 * have two optimization chances, 1) when caller doesn't need
+	 * delta_exec and 2) when the task's delta_exec is 0. The former is
+	 * obvious. The latter is complicated. reading ->on_cpu is racy, but
+	 * this is ok. If we race with it leaving cpu, we'll take a lock. So
+	 * we're correct. If we race with it entering cpu, unaccounted time
+	 * is 0. This is indistinguishable from the read occurring a few
+	 * cycles earlier.
+	 */
+	if (!add_delta || !p->on_cpu)
+		return p->se.sum_exec_runtime;
+#endif
+
 	rq = task_rq_lock(p, &flags);
 	ns = p->se.sum_exec_runtime;
 	if (add_delta)
